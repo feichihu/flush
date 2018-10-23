6518ab44dc07433dac256ba5bb0094d4f5985d42
SatatOjha <ojhasata@umich.edu>
2018-10-23 12:51:30 -0400

    completed

--- Mastik/python_plotting/plot_cycles.py
+++ Mastik/python_plotting/plot_cycles.py
@@ -10,14 +10,29 @@ lists.append(list1)
 lists.append(list2)
 lists.append(list3)
 
+max_num = 0
 
 t = 0
 
-with open("../demo/out", "r") as F:
-    for line in F:
-        time_step.append(t)
-        t = t + 1
-        for word in line.split():
-            w = 0;
-            lists[w].append(word)
-            w = w + 1
+with open("out.txt", "r") as F: #change based on directory of output file
+	for line in F:
+		time_step.append(t)
+		t = t + 1
+		w = 0
+		for word in line.split():
+			num = int(word)
+			if(num > max_num):
+				max_num = num
+
+			lists[w].append(num)
+			w = w + 1
+
+time_step_len = len(time_step)
+
+#print("time_step size: ", len(time_step))
+#print("list 1 size: ", len(lists[1]))
+#print("max num is: ", max_num)
+
+plt.plot(time_step, lists[0], 'r--', time_step, lists[1], 'bs', time_step, lists[2], 'g^')
+plt.axis([0, time_step_len, 0, max_num])
+plt.show()
